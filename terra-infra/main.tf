cc20e07e89bf52de86b6cbfa7799edefb720171e
Hemachandra-Menakuru <hemachandrareddy.m@gmail.com>
2022-08-14 18:20:09 +1200

    Updated Terraform code : 21

--- terra-infra/main.tf
+++ terra-infra/main.tf
@@ -11,15 +11,14 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+locals {
+  test = abspath(path.root)
+}
+output "test" { value = "${local.test}" }
+
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "test-bkt-dply-terraform"
   tags = {
     Name = "Created and deployed this bucket through terraform"
   }
-}
-
-resource "aws_s3_object" "object1" {
-  bucket = aws_s3_bucket.test_bucket.id
-  key = "test2.txt"
-  source = "/aws/s3/test2.txt"
 }
