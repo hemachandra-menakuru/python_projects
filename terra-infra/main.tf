e411544668b15009c2cc088e9316c9706b3937d9
Hemachandra-Menakuru <hemachandrareddy.m@gmail.com>
2022-08-14 20:28:18 +1200

    Updated Terraform code : 29

--- terra-infra/main.tf
+++ terra-infra/main.tf
@@ -28,3 +28,9 @@ resource "aws_s3_bucket" "test_bucket" {
     Name = "Created and deployed this bucket through terraform"
   }
 }
+
+resource "aws_s3_object" "object1" {
+  bucket = aws_s3_bucket.test_bucket.id
+  key = "test2.txt"
+  source = "../../aws/s3/test2.txt"
+}
