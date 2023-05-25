bcb888e0df3085b52698253223ba4f801a4d797d
Hemachandra Menakuru <90375082+hemachandra-menakuru@users.noreply.github.com>
2023-05-25 13:04:00 +1200

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -26,3 +26,19 @@ SELECT
   '</employees>' AS xml_data
 FROM
   employees;
+
+    
+    -----------
+    
+    SELECT CONCAT('Column_X_Match_Count:', COUNT(CASE WHEN Column_X = 'MATCH' THEN 1 END)) AS Counts
+FROM data
+UNION ALL
+SELECT CONCAT('Column_X_NoMatch_Count:', COUNT(CASE WHEN Column_X = 'NOMATCH' THEN 1 END)) AS Counts
+FROM data
+UNION ALL
+SELECT CONCAT('Column_Y_Match_Count:', COUNT(CASE WHEN Column_Y = 'MATCH' THEN 1 END)) AS Counts
+FROM data
+UNION ALL
+SELECT CONCAT('Column_Y_NoMatch_Count:', COUNT(CASE WHEN Column_Y = 'NOMATCH' THEN 1 END)) AS Counts
+FROM data;
+
