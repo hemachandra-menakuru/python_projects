5c3ffa4286e4f71be36aff77f131e8805c02fdc7
Hemachandra Menakuru <90375082+hemachandra-menakuru@users.noreply.github.com>
2023-07-04 14:59:45 +1200

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -11,3 +11,8 @@ def fun_test(val1):
     return x
     
 fun_test(val1)
+
+
+--------
+
+Hi Doug, I have observed something interesting, and in fact, dangerous, while discussing with Richelle. There is an ETL role called ETL_DATA_ROLE, which is specifically designed for service accounts. Informatica and Airflow service accounts use this role to transform data, and it has permissions to INSERT/UPDATE/DELETE/TRUNCATE data. However, user accounts are now using this role through AD Roles. This means that anyone who has access to this role can perform all operations through the Snowflake UI. While no one may intentionally cause harm, the chances of accidental actions are very high. This can lead to data irregularities and make it difficult to analyze failures. We may need to raise this issue with BDH and discuss the removal of the AD role from the domain. We should also consider alternative roles for granting users.
